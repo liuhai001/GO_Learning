5f040165d4292683237f0af0c52a089a8053bcf9
liuhai <944895050@qq.com>
2018-05-27 09:51:13 +0800

    你好世界

---
+++ hello.go
@@ -0,0 +1,11 @@
+//第一个GO语言程序
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello World！;你好，世界！")
+}
---
+++ reflect.go
@@ -0,0 +1,40 @@
+//反射中调用方法
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"strconv"
+)
+
+type Mytype struct {
+	i    int
+	Name string
+}
+
+func (m *Mytype) SetI(i int) {
+	m.i = i
+}
+
+func (m *Mytype) SetName(name string) {
+	m.Name = name
+}
+
+func (m *Mytype) Sprint() string {
+	return fmt.Sprintf("%p:", m) + "--name:" + m.Name + "--i:" + strconv.Itoa(m.i)
+}
+
+func main() {
+	mytype := &Mytype{26, "LIUHAI"}
+	fmt.Println(mytype)
+	mtV := reflect.ValueOf(&mytype).Elem()
+	fmt.Println("Before:", mtV.MethodByName("Sprint").Call(nil)[0])
+
+	params := make([]reflect.Value, 1)
+	params[0] = reflect.ValueOf(18)
+	mtV.MethodByName("SetI").Call(params)
+	params[0] = reflect.ValueOf("reflection test")
+	mtV.MethodByName("SetName").Call(params)
+	fmt.Println("After:", mtV.MethodByName("Sprint").Call(nil)[0])
+
+}
